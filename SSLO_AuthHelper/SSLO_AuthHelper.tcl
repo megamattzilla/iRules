d4a68b50ba146280335d4b79fa705677917817d7
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2019-06-12 10:53:00 -0500

    disabled logging and debug by default

--- SSLO_AuthHelper/SSLO_AuthHelper.tcl
+++ SSLO_AuthHelper/SSLO_AuthHelper.tcl
@@ -18,19 +18,17 @@ set noauth "sslo_noauth.app/sslo_noauth-xp-4"
 set key "authstatus"
 
 ##Optional Debug mode to clear table entries##
-switch [HTTP::uri]  {
-  "http://neverssl.com/debug" {
-  table delete -subtable "[IP::client_addr]" $key
-  table delete -subtable "[IP::client_addr]" attempt
-	log local0. "Purged table entries for [IP::client_addr]"
-  }
-}
-#table delete -subtable "[IP::client_addr]" $key
-#table delete -subtable "[IP::client_addr]" attempt
+#switch [HTTP::uri]  {
+#  "http://neverssl.com/debug" {
+#  table delete -subtable "[IP::client_addr]" $key
+#  table delete -subtable "[IP::client_addr]" attempt
+#	log local0. "Purged table entries for [IP::client_addr]"
+#  }
+#}
 
 #set variable called authlookup with the current authstatus of this session 
 set authlookup [table lookup -subtable [IP::client_addr] $key]
-log local0. "client IP [IP::client_addr] auth status is $authlookup for URI [HTTP::uri]"
+#log local0. "client IP [IP::client_addr] auth status is $authlookup for URI [HTTP::uri]"
 #Definition of all authstatus
 #1 = known ntlm client
 #2 = known kerberos client
@@ -41,44 +39,44 @@ log local0. "client IP [IP::client_addr] auth status is $authlookup for URI [HTT
 switch $authlookup  {
   "1" {
     virtual $ntlm
-	log local0. "known IP [IP::client_addr] sent to VS $ntlm"
+#	log local0. "known IP [IP::client_addr] sent to VS $ntlm"
 	return
   }
   "2" {
     virtual $kerberos
-	log local0. "known IP [IP::client_addr] sent to VS $kerberos"
+#	log local0. "known IP [IP::client_addr] sent to VS $kerberos"
 	return
   }
   "3" {
 	virtual $noauth
-	log local0. "known IP [IP::client_addr] sent to VS $noauth"
+#	log local0. "known IP [IP::client_addr] sent to VS $noauth"
 	return
   } 
 }
 
 #set variable called attempt with the current auth attempt number for this client  
 set attempt [table lookup -subtable [IP::client_addr] attempt]
-log local0. "client IP [IP::client_addr] starting auth attempt $attempt"
+#log local0. "client IP [IP::client_addr] starting auth attempt $attempt"
 
 #unknown user lookup
 switch -glob "$authlookup|[HTTP::header "Proxy-Authorization"]|$attempt"  {
   "4|NTLM*|*" {
     table set -subtable "[IP::client_addr]" $key 1
 	virtual $ntlm
-	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $ntlm"
+#	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $ntlm"
   }
   "4|Negotiate*|*" {
 	table set -subtable "[IP::client_addr]" $key 2
 	virtual $kerberos
-	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $kerberos"
+#	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $kerberos"
   }
   "4|*|4" {
 	table set -subtable "[IP::client_addr]" $key 3
 	virtual $noauth
-	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $noauth"
+#	log local0. "discovered IP [IP::client_addr] with header [HTTP::header "Proxy-Authorization"] sent to VS $noauth"
   }  
   default {
-	log local0. "new IP [IP::client_addr] sending HTTP 407"
+#	log local0. "new IP [IP::client_addr] sending HTTP 407"
 	table incr -subtable "[IP::client_addr]" attempt
 	table set -subtable "[IP::client_addr]" $key 4
     HTTP::respond 407 -version auto content "<html><title>Authentication Required</title><body>Error: Authentication Failure</body></html>" Proxy-Authenticate "Negotiate" Proxy-Authenticate "NTLM" Proxy-Authenticate "Basic"
