b5baccd64ac3e795a308d53e4a2e8297a7c7ea7d
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2019-07-02 16:57:58 -0500

    updated exception handling

--- SSLO_Auth_user_and_group/Add_user_and_group.tcl
+++ SSLO_Auth_user_and_group/Add_user_and_group.tcl
@@ -1,11 +1,8 @@
 when HTTP_REQUEST {
+    if { [HTTP::header exists X-Authenticated-User] } {
+        HTTP::header remove X-Authenticated-User
+    }
     if { [ACCESS::session exists] } {
-        set user [ACCESS::session data get "session.logon.last.username"]
-		set groups [ACCESS::session data get "session.ad.last.attr.memberOf"]
-		#Optional XFF header. 
-        #set cip [IP::client_addr]
-        HTTP::header insert "X-Authenticated-User" $user
-		HTTP::header insert "X-Authenticated-Groups" $groups
-		#HTTP::header insert "X-Forwarded-For" "$cip"
+        HTTP::header insert "X-Authenticated-User" [ACCESS::session data get "session.logon.last.username"]
     }
 }
