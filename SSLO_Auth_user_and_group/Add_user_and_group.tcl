682404480651b62694bb823f870032336af594b4
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2019-08-01 18:56:03 -0500

    fixed irule syntax

--- SSLO_Auth_user_and_group/Add_user_and_group.tcl
+++ SSLO_Auth_user_and_group/Add_user_and_group.tcl
@@ -1,14 +1,14 @@
 when HTTP_REQUEST {
-   # loop through and remove all instances of the unwanted
-   # headers from the server response
-   # (X-Authenticated-User, X-Authenticated-Group in this example)
-   foreach header {X-Authenticated-User X-Authenticated-Group} {
-      log local0. "Removing $header: [HTTP::header value $header]"
-      HTTP::header remove $header
-   }
-   # If access session exists for this flow, insert the X-Authenticated-User and X-Authenticated-Groups HTTP headers 
-   if { [ACCESS::session exists] } {
+    if { [HTTP::header exists X-Authenticated-User] } {
+        HTTP::header remove X-Authenticated-User
+    }
+    if { [HTTP::header exists X-Authenticated-Groups] } {
+        HTTP::header remove X-Authenticated-Groups
+    }
+    if { [string length [ACCESS::session data get "session.logon.last.username"]] > 0  } {
         HTTP::header insert "X-Authenticated-User" [ACCESS::session data get "session.logon.last.username"]
+    }
+    if { [string length [ACCESS::session data get "session.ad.last.attr.memberOf"]] > 0 } {
         HTTP::header insert "X-Authenticated-Groups" [ACCESS::session data get "session.ad.last.attr.memberOf"]
-   }
+    }
 }
--- SSLO_Auth_user_and_group/Remove_user_and_group.tcl
+++ SSLO_Auth_user_and_group/Remove_user_and_group.tcl
@@ -1,9 +1,8 @@
 when HTTP_REQUEST {
-   # loop through and remove all instances of the unwanted
-   # headers from the server response
-   # (X-Authenticated-User, X-Authenticated-Groups in this example)
-   foreach header {X-Authenticated-User X-Authenticated-Groups} {
-      log local0. "Removing $header: [HTTP::header value $header]"
-      HTTP::header remove $header
-   }
-}
+    if { [HTTP::header exists X-Authenticated-User] } {
+        HTTP::header remove X-Authenticated-User
+    }
+    if { [HTTP::header exists X-Authenticated-Groups] } {
+        HTTP::header remove X-Authenticated-Groups
+    }
+}    
