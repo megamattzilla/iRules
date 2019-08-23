2139eb076cf315d31ed014dde5ad81133f47954c
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2019-08-23 16:30:43 -0500

    added comments to cert verify with grace period

--- SSLO_Custom_Cert_Verify/interceptionvs_certverity_graceperiod.tcl
+++ SSLO_Custom_Cert_Verify/interceptionvs_certverity_graceperiod.tcl
@@ -1,3 +1,4 @@
+##Place on SSLO interception -in-t-4 rule to enable custom origin certificate checks.  
 when SERVERSSL_HANDSHAKE {
     if { [info exists cert_error] } {
         SSL::forward_proxy cert response_control mask
@@ -5,6 +6,7 @@ when SERVERSSL_HANDSHAKE {
 }
 when SERVERSSL_SERVERCERT {
     if { [X509::verify_cert_error_string [SSL::verify_result]] ne "ok" } {
+        ##If any certificate errors are found, enable certificate masking and set a shared variable with the certificate status 
         set cert_error 1
         sharedvar ctx
         set ctx(cert_control) [X509::verify_cert_error_string [SSL::verify_result]]
@@ -13,6 +15,8 @@ when SERVERSSL_SERVERCERT {
         set now [clock seconds]
         set offset 604800 ; ## 7 days
         if { [expr { ( ${now} - [clock scan [X509::not_valid_after [SSL::cert 0]]] ) < ${offset} }] } {
+            ##If the only problem with this certificate is that it expired in 7 days or less, mask the cert and\
+            #unset the ctc(cert_control) variable so no HTTP headers will get inserted by the second iRule. 
             set cert_error 1
             unset ctx(cert_control)
             }
