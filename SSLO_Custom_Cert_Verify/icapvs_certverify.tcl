29bd2a8577be0705e88b8fbc2ea966159fceb4b2
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2019-08-23 16:48:21 -0500

    updated icapvs cert verify irule

--- SSLO_Custom_Cert_Verify/icapvs_certverify.tcl
+++ SSLO_Custom_Cert_Verify/icapvs_certverify.tcl
@@ -12,7 +12,7 @@ when HTTP_DISABLED {
 when HTTP_REQUEST {
     sharedvar ctx
     if { ( [info exists ctx(cert_control)] ) and ( $ctx(cert_control) ne "" ) } {
-        HTTP::header insert "X-Origin-Certstatus" $ctx(cert_control)
-        log local0. "cert status: $ctx(cert_control)"
+        HTTP::header insert "X-Origin-BlockCertificate" reason $ctx(cert_control)
+        #log local0. "X-Origin-BlockCertificate: reason $ctx(cert_control)"
         }
 }
