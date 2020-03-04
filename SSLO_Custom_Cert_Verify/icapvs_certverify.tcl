9ae65222adfa44f4ca93959ad88c0736a1e03abb
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2020-03-04 15:41:24 -0600

    Update icapvs_certverify.tcl
    fixed issue with iRule validation missing quotes

--- SSLO_Custom_Cert_Verify/icapvs_certverify.tcl
+++ SSLO_Custom_Cert_Verify/icapvs_certverify.tcl
@@ -1,7 +1,7 @@
 when HTTP_REQUEST {
     sharedvar ctx
     if { ( [info exists ctx(cert_control)] ) and ( $ctx(cert_control) ne "" ) } {
-        HTTP::header insert "X-Origin-BlockCertificate" reason $ctx(cert_control)
+        HTTP::header insert "X-Origin-BlockCertificate" "reason $ctx(cert_control)"
         #log local0. "X-Origin-BlockCertificate: reason $ctx(cert_control)"
         }
 }
