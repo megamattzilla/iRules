ca46a2fea7f61f32bab947bb0c6613eb75e2353c
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2020-07-28 14:53:25 -0500

    updated iRule to check for username vs http codes

--- SSLO_Multiple_Auth/SSLO_vip_target_vip_multiple_auth.tcl
+++ SSLO_Multiple_Auth/SSLO_vip_target_vip_multiple_auth.tcl
@@ -1,5 +1,6 @@
 when RULE_INIT priority 200 {
 ###User-Edit Variables start###
+
 #Specify name for Captive Portal SSLO topology
 set static::prod_portal_sslo "sslo_prod_portal.app/sslo_prod_portal-xp-4"
 #Specify name for Kerberos SSLO topology
@@ -7,26 +8,19 @@ set static::prod_kerberos_sslo "sslo_prod_kerb.app/sslo_prod_kerb-xp-4"
 #Specify name for no-auth SSLO topology
 set static::prod_noauth_sslo "sslo_prod_noauth.app/sslo_prod_noauth-xp-4"
 #Specify authentication preference idle timeout 
-set static::prod_idle_sec_timeout "30" 
+set static::prod_idle_sec_timeout "900" 
 #Specify datagroup to use for client IP authentication bypass
 set static::noauth_datagroup_ip "bypass_auth_ips"
-#Specify number of HTTP responses to collect to use for pass/fail decisions
-set static::noauth_HTTPtocount "4"
-#Specify (in seconds) rolling window of HTTP responses to collect to use for pass/fail decisions
-set static::noauth_rollingwindow "10"
-#Specify ratio of HTTP Status code 407s:200s to use for pass/fail decisions. 
-set static::noauth_ratio "3"
 #Specify if debug logging should be enabled. 1 = enabled, 0 = disabled.    
 set static::debuglog "1"
-#Example if APM Kerberos access profile is set to 3 auth attempts, it will generally send ~3 HTTP 407s until it fails open. After successful/fail open Kerberos HTTP status code will change to 200 Connected.  
-#So assuming 3:1 will be a failure- 3 divided by 1 equals 3. This variable sets the number that will be checked when we divide 407s by 200s.  
-#In practice a failed kerberos session will have high ratio of 407s to 200s (>3) and a successful kerberos session will have a log ratio of 407s to 200s (<3) 
+
 ###User-Edit Variables end###
 }
 
 when HTTP_REQUEST priority 200 {
 
-if { $static::debuglog } { log local0. "DEBUG: CPU [TMM::cmp_unit] VS [virtual] 5tuple [client_addr]:[client_port] -> [IP::local_addr]:[TCP::local_port] proto [IP::protocol] for [HTTP::host][HTTP::uri] SID [ACCESS::session sid] Access profile [ACCESS::session data get "session.access.profile"] user [ACCESS::session data get "session.logon.last.username"] accesssid [table lookup -subtable [IP::client_addr] accesssid]" 
+#Debug Logging for troubleshooting.Should be typically disabled. 
+if { $static::debuglog } { log local0. "DEBUG: CPU [TMM::cmp_unit] VS [virtual] 5tuple [client_addr]:[client_port] -> [IP::local_addr]:[TCP::local_port] proto [IP::protocol] for [HTTP::host][HTTP::uri] SID [ACCESS::session sid] Access profile [ACCESS::session data get "session.access.profile"] user [ACCESS::session data get "session.logon.last.username"] accesssid [table lookup -subtable [IP::client_addr] accesssid] kerbaccesssid [table lookup -subtable [IP::client_addr] kerbaccesssid]" 
  }
 
 
@@ -44,7 +38,7 @@ if { $static::debuglog } { log local0. "client IP [IP::client_addr] auth status 
 switch [HTTP::uri]  {
   "http://neverssl.com/debug" {
   table delete -subtable "[IP::client_addr]" authstatus
-  HTTP::respond 301 Location "http://neverssl.com"
+  HTTP::respond 302 Location "http://neverssl.com" cache-control "no-store"
   if { $static::debuglog } { log local0. "Purged table entries for [IP::client_addr] and redirected to new page" }
   return
   }
@@ -60,98 +54,65 @@ if { [class match [IP::client_addr] equals "$static::noauth_datagroup_ip" ] } {
 }
 
 
-#known user lookup
+#known user lookup. After successful Kerberos or captive portal sessions most HTTP requests will be handled here. 
 switch $authlookup  {
     "2" {
-    #set the SSLO Kerberos/Basic virtual server name
+    #Found known kerberos client. direct request to SSLO Kerberos/Basic virtual server name and exit iRule. 
     virtual $static::prod_kerberos_sslo
     if { $static::debuglog } { log local0. "known IP [IP::client_addr] sent to VS kerberos" }
 	return
   }
   "4" {
-    #set the SSLO Captive Portal virtual server name
+    #Found known captive portal client. direct request to SSLO Captive Portal virtual server and exit iRule. 
     virtual $static::prod_portal_sslo
     if { $static::debuglog } { log local0. "known IP [IP::client_addr] sent to VS captive portal" }
 	return
   }
   "5" {
-    #set the no auth virtual server name
+    #Found known no-auth client. direct request to SSLO no-auth virtual server and exit iRule. 
 	virtual $static::prod_noauth_sslo
     if { $static::debuglog } { log local0. "known IP [IP::client_addr] sent to VS fallback noauth" }
 	return
   } 
 }
 
-#Send captive portal traffic to the captive portal VS- bypassing the HTTP Proxy. 
-if { [HTTP::host] == "ssloprxy.f5kc.lab.local:4001" } {
-    if { $static::debuglog } { log local0. "Sending to captive portal VS" }
-    virtual auth2.0_captive_portal
-    return
-}
-
 #If we are trying captive portal for a client, do that now. 
 if { $authlookup == 3 } {
+        #Found trying captive portal session
         if { $static::debuglog } { log local0. "Trying captive portal auth for client IP [IP::client_addr]" }
-        #As of this version we dont check the captive portal clients- we expect it to fail open so set their auth status to captive portal. Add Captive portal check here in future if needed. 
+        #As of this version we dont check the captive portal clients for success/fail criteria. We expect clients to handle the redirect and the access policy to fail open so set their auth status to known captive portal. Add Captive portal check here in future if needed. 
         table set -subtable "[IP::client_addr]" authstatus 4 $static::prod_idle_sec_timeout
         virtual $static::prod_portal_sslo
         return
 }
 
-#After 4 HTTP responses have been observed check the Response Ratio of 407s to 200s
-set 407s [table keys -count -subtable "407reqrate:[IP::client_addr]"]
-set 200s [table keys -count -subtable "200reqrate:[IP::client_addr]"]
-if { $407s + $200s > $static::noauth_HTTPtocount } {
-    #If no 200s or no 407s have been sent, reset the value to .1. 
-    if { $407s == 0 } { set 407s .1 } 
-    if { $200s == 0 } { set 200s .1 } 
-    #Required number of HTTP responses have been sent to client. Looking at ratio of 407s to 200s. 
-    if { $407s / $200s >= $static::noauth_ratio } {
-        #Kerberos Pass Condition 
-        if { $static::debuglog } { log local0. "Kerberos Failed. Try captive portal auth for client IP [IP::client_addr]" }
-        virtual $static::prod_portal_sslo
-        table set -subtable "[IP::client_addr]" authstatus 3 $static::prod_idle_sec_timeout
-        return
-        } elseif { $407s / $200s <= 3 } { 
-        #Kerberos Fail Confidition
-        if { $static::debuglog } { log local0. "Kerberos Passed for client IP [IP::client_addr]" }
-        virtual $static::prod_kerberos_sslo
-        table set -subtable "[IP::client_addr]" authstatus 2 $static::prod_idle_sec_timeout
-        return
-    }
+#Check if we are trying kerberos auth for a client. 
+if { [table lookup -subtable [IP::client_addr] authstatus] == 1 } {
+    #Confirmed we are trying kerberos. Check if Kerberos access agent has been triggered and we know the kerberos access session SID
+    if { [string length [table lookup -subtable [IP::client_addr] kerbaccesssid]] > 0 } {
+        #Kerberos SID found. Check if we found a username for that sid
+        if { [string length [ACCESS::session data get -sid [table lookup -subtable [IP::client_addr] kerbaccesssid] "session.logon.last.username"]] < 1 } {
+            #No User identified. Delete kerberos access session and try captive portal auth. 
+            ACCESS::session remove -sid [table lookup -subtable [IP::client_addr] kerbaccesssid]
+            virtual $static::prod_portal_sslo
+            table set -subtable "[IP::client_addr]" authstatus 3 $static::prod_idle_sec_timeout
+            if { $static::debuglog } { log local0. "Kerberos Failed. Try captive portal auth for client IP [IP::client_addr]. Removed kerberos access sid was [table lookup -subtable [IP::client_addr] kerbaccesssid]" }
+            #Cleanup kerbaccesssid that we deleted the corresponding APM session. 
+            table delete -subtable [IP::client_addr] kerbaccesssid
+            return
+        } elseif { [string length [ACCESS::session data get -sid [table lookup -subtable [IP::client_addr] kerbaccesssid] "session.logon.last.username"]] > 0 } {
+            #User identified from kerberos. Mark client IP as known kerberos user. 
+            if { $static::debuglog } { log local0. "Kerberos Passed for client IP [IP::client_addr] user [ACCESS::session data get -sid [table lookup -subtable [IP::client_addr] kerbaccesssid] "session.logon.last.username"]" }
+            virtual $static::prod_kerberos_sslo
+            table set -subtable "[IP::client_addr]" authstatus 2 $static::prod_idle_sec_timeout
+            return 
+        }
+    } 
 }
-
-
 
 #Fallback to Kerberos auth. Probably first HTTP requests from client.   
 virtual $static::prod_kerberos_sslo
 table set -subtable "[IP::client_addr]" authstatus 1 $static::prod_idle_sec_timeout
-if { $static::debuglog } { log local0. "Client IP [IP::client_addr] trying kerberos auth" }
+if { $static::debuglog } { log local0. "Fallback: Client IP [IP::client_addr] trying kerberos auth" }
 return
-}
-
-
-when HTTP_RESPONSE priority 200 { 
-
-#Disable checking status code if its a Kerberos or Captive portal user. 
-if { [table lookup -subtable [IP::client_addr] authstatus] == 2 || [table lookup -subtable [IP::client_addr] authstatus] == 4 } {
-return
-}
-
-switch [HTTP::status]  {
-    "407" {
-    #Create a rolling 10 second window and increment for each HTTP 407 response
-    set 407reqno [table incr "407reqs:[IP::client_addr]"]
-    table set -subtable 407reqrate:[IP::client_addr] $407reqno ignore 30 $static::noauth_rollingwindow 
-    if { $static::debuglog } { log local0. "found HTTP 407 for Client IP [IP::client_addr]" }
-	return
-  }
-  "200" {
-    #Create a rolling 10 second window and increment for each HTTP 200 response
-    set 200reqno [table incr "200reqs:[IP::client_addr]"]
-    table set -subtable 200reqrate:[IP::client_addr] $200reqno ignore 30 $static::noauth_rollingwindow 
-    if { $static::debuglog } { log local0. "found HTTP 200 for Client IP [IP::client_addr]" }
-  }
-}
-
 }
