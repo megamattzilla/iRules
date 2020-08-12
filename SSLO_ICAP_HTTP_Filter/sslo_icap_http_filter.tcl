99949d7494bb532da57d74d06b1f533742f623d8
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2020-08-12 16:27:11 -0500

    updated readme

--- SSLO_ICAP_HTTP_Filter/sslo_icap_http_filter.tcl
+++ SSLO_ICAP_HTTP_Filter/sslo_icap_http_filter.tcl
@@ -7,11 +7,13 @@ if { ([HTTP::method] == "POST" ) || ([HTTP::method] == "PUT" ) }  {
     if { ([HTTP::header "Content-Length"] <= "1096" ) || ([HTTP::header "Content-Length"] >= "52428800") } {
         #HTTP request is type POST or PUT but smaller than 1096 or larger than 50MB. Bypass ICAP for this request
         log local0. "Bypassing ICAP for IP [IP::client_addr] HTTP [HTTP::host] [HTTP::uri] [HTTP::method] content-length [HTTP::header "content-length"]"
+        #Disable ICAP request mod profile. For some reason the command is ADAPT::enable but the 0 means its disable (1 = enable). 
         ADAPT::enable request 0 
         }
 } else {
 #If HTTP request method is NOT POST or PUT do something here.
 log local0. "Fallback: Bypassing ICAP for IP [IP::client_addr] HTTP [HTTP::host] [HTTP::uri] [HTTP::method]"
+#Disable ICAP request mod profile. For some reason the command is ADAPT::enable but the 0 means its disable (1 = enable). 
 ADAPT::enable request 0 
 }
 }
