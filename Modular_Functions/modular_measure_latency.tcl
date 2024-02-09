f811ef9f396e134d55b8dcedf62a55e3c818d9ba
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2024-02-09 15:03:22 -0600

    adding modular function iRules

---
+++ Modular_Functions/modular_measure_latency.tcl
@@ -0,0 +1,218 @@
+# Made with ❤ by Matt Stovall 2/2024. 
+# This iRule calculates latency at various TCP, TLS, HTTP, ASM events and caculates the latency between each stage. Others iRules can query this information stored in variables. 
+#All code is wrapped in catch statements so that any failure will be non-blocking. If making changes to the code, please ensure its still covered by the catch statements. 
+#See https://github.com/megamattzilla/iRules/blob/master/Modular_Functions/README.md for more details
+#Version 1.0
+#Modular iRule dependency: none
+
+#Requirements: 
+#Event CLIENTSSL_HANDSHAKE requires clientssl profile on virtual
+#Event SERVERSSL_HANDSHAKE requires serverssl profile on virtual
+#Requires ASM profile on virtual server with iRule events enabled (this is common for most ASM deployments)
+
+when FLOW_INIT priority 100 {
+
+    ###User-Edit Variables start###
+    set mml_measureWithoutHeader 0 ; #0 = Measure all HTTP requests, 1 = only measure latency for HTTP requests that contain HTTP header name $mml_clientEnableTimingHeaderName and header value $mml_clientEnableTimingHeaderValue
+    set mml_clientEnableTimingHeaderName X-Enable-Server-Timing ; #Client HTTP header name that triggers debug timing to take place.
+    set mml_clientEnableTimingHeaderValue 1 ; #Recommend an integer. Client HTTP header value that triggers debug timing to take place. 
+    set mml_enableInsertResponseHeader 1 ; #1 = enabled, 0 = disabled. Insert response HTTP header named $mml_serverTimingHeaderName with server-timing data. 
+    set mml_serverTimingHeaderName Server-Timing ; #specify the HTTP response header name to be inserted.
+    set mml_serverTimingLabel f5 ; #specify the root label name in server timing value. Example: "Server-Timing: $thisvalue, overhead;dur=1, origin;dur=2"
+    ###User-Edit Variables end###
+
+    #Don't edit these system variables 
+    set mml_FLOW_INIT [clock clicks -milliseconds]
+    set mml_CLIENT_ACCEPTED 0
+    set mml_CLIENTSSL_CLIENTHELLO 0
+    set mml_CLIENTSSL_HANDSHAKE 0 
+    set mml_http_request_count 0
+    set mml_HTTP_REQUEST 0 
+    set mml_ASM_REQUEST_DONE 0 
+    set mml_LB_SELECTED 0 
+    set mml_HTTP_REQUEST_RELEASE 0 
+    set mml_SERVER_CONNECTED 0 
+    set mml_SERVERSSL_CLIENTHELLO_SEND 0 
+    set mml_SERVERSSL_HANDSHAKE 0 
+    set mml_HTTP_RESPONSE 0 
+    set mml_HTTP_RESPONSE_RELEASE 0
+    set mml_debugTiming 0
+}
+
+when CLIENT_ACCEPTED priority 10 {
+set mml_CLIENT_ACCEPTED [clock clicks -milliseconds] 
+}
+when CLIENTSSL_CLIENTHELLO priority 10 {
+set mml_CLIENTSSL_CLIENTHELLO [clock clicks -milliseconds] 
+}
+when CLIENTSSL_HANDSHAKE priority 10 {
+set mml_CLIENTSSL_HANDSHAKE [clock clicks -milliseconds] 
+}
+
+when HTTP_REQUEST priority 10 {
+
+#Check if this HTTP request indicates further timing events should be collected. 
+if { $mml_measureWithoutHeader equals 1 or [HTTP::header value $mml_clientEnableTimingHeaderName ] equals $mml_clientEnableTimingHeaderValue } {
+    log local0. "measuring latency!!"
+    set mml_debugTiming 1
+    incr mml_http_request_count
+    set mml_HTTP_REQUEST [clock clicks -milliseconds]
+} 
+}
+
+
+#Requires ASM profile to have iRule events enabled to populate data. It wont cause failures if its disabled in the ASM policy and still uncommented here but it will cause stat collection to be skipped.   
+when ASM_REQUEST_DONE priority 10  {
+  if { $mml_debugTiming equals 1 } { set mml_ASM_REQUEST_DONE [clock clicks -milliseconds] } 
+}
+when LB_SELECTED priority 10  {
+ if { $mml_debugTiming equals 1 } { set mml_LB_SELECTED [clock clicks -milliseconds] } 
+}
+when SERVER_CONNECTED priority 10  {
+ if { $mml_debugTiming equals 1 } { set mml_SERVER_CONNECTED [clock clicks -milliseconds] }
+}
+when SERVERSSL_CLIENTHELLO_SEND priority 10  {
+ if { $mml_debugTiming equals 1 } { set mml_SERVERSSL_CLIENTHELLO_SEND [clock clicks -milliseconds] } 
+}
+when SERVERSSL_HANDSHAKE priority 10  {
+if { $mml_debugTiming equals 1 } { set mml_SERVERSSL_HANDSHAKE [clock clicks -milliseconds] } 
+}
+when HTTP_REQUEST_RELEASE priority 10  {
+ if { $mml_debugTiming equals 1 } { set mml_HTTP_REQUEST_RELEASE [clock clicks -milliseconds] } 
+}
+when HTTP_RESPONSE priority 10 {
+ if { $mml_debugTiming equals 1 } { set mml_HTTP_RESPONSE [clock clicks -milliseconds] } 
+}
+
+when HTTP_REQUEST priority 1000 {
+    #Run at priority 1000 (very last) to see if another iRule has responded to the HTTP request. If so, generate partial latency data. 
+    
+    #Exit gracefully if request does not contain required server timing enable header.
+    if { $mml_debugTiming equals 0 } {
+    return
+    }
+
+    #If another iRule has responded to this request, calculate the stats we have collected so far.
+    if {[HTTP::has_responded]} {
+    
+    # Check if this is the first http request In this TCP session. If its a re-used TCP session, we have no helpful latency data for this request and just exit gracefully. 
+    if { $mml_http_request_count equals 1 } {
+
+        set mml_a [expr { $mml_CLIENT_ACCEPTED - $mml_FLOW_INIT } ] ; #measure time spent in Client TCP 3WHS. 
+        set mml_b [expr { $mml_CLIENTSSL_HANDSHAKE - $mml_CLIENTSSL_CLIENTHELLO } ] ; #measure time spent in client side ssl handshake.
+    }
+    #Clean up variables for next HTTP request incase there is TCP reuse. 
+    #Dont edit these system Variables 
+    set mml_FLOW_INIT [clock clicks -milliseconds]
+    set mml_CLIENT_ACCEPTED 0
+    set mml_CLIENTSSL_CLIENTHELLO 0
+    set mml_CLIENTSSL_HANDSHAKE 0 
+    set mml_HTTP_REQUEST 0 
+    set mml_ASM_REQUEST_DONE 0 
+    set mml_HTTP_REQUEST_RELEASE 0 
+    set mml_SERVER_CONNECTED 0 
+    set mml_SERVERSSL_CLIENTHELLO_SEND 0 
+    set mml_SERVERSSL_HANDSHAKE 0  
+    set mml_HTTP_RESPONSE 0 
+    set mml_HTTP_RESPONSE_RELEASE 0
+    set mml_debugTiming 0
+     }
+
+}
+
+
+when ASM_REQUEST_BLOCKING priority 10 {
+    #Requires ASM policy "raise iRule event" setting to be enabled in ASM policy. This event is raised when ASM has triggered a block action for the request.
+    #Exit gracefully if request does not contain required server timing enable header.
+
+    if { $mml_debugTiming equals 0 } {
+    return
+    }
+    # collect additional connection level stats if this is the first http request In this TCP session. 
+    if { $mml_http_request_count equals 1 } {
+
+        set mml_a [expr { $mml_CLIENT_ACCEPTED - $mml_FLOW_INIT } ] ; #measure time spent in Client TCP 3WHS. 
+        set mml_b [expr { $mml_CLIENTSSL_HANDSHAKE - $mml_CLIENTSSL_CLIENTHELLO } ] ; #measure time spent in client side ssl handshake.
+        set mml_c [expr { $mml_ASM_REQUEST_DONE - $mml_HTTP_REQUEST } ] ; #measure time spent in F5 HTTP request processing time.
+
+} else {
+        set mml_c [expr { $mml_ASM_REQUEST_DONE - $mml_HTTP_REQUEST } ] ; #measure time spent in F5 HTTP request processing time.
+        }
+    #Clean up variables for next HTTP request incase there is TCP reuse. 
+    #Dont edit these system Variables 
+    set mml_FLOW_INIT [clock clicks -milliseconds]
+    set mml_CLIENT_ACCEPTED 0
+    set mml_CLIENTSSL_CLIENTHELLO 0
+    set mml_CLIENTSSL_HANDSHAKE 0 
+    set mml_HTTP_REQUEST 0 
+    set mml_ASM_REQUEST_DONE 0 
+    set mml_HTTP_REQUEST_RELEASE 0 
+    set mml_SERVER_CONNECTED 0 
+    set mml_SERVERSSL_CLIENTHELLO_SEND 0 
+    set mml_SERVERSSL_HANDSHAKE 0  
+    set mml_HTTP_RESPONSE 0 
+    set mml_HTTP_RESPONSE_RELEASE 0
+    set mml_debugTiming 0
+     }
+
+when HTTP_RESPONSE_RELEASE priority 520 {
+    #Exit gracefully if request does not contain required server timing enable header.
+
+    if { $mml_debugTiming equals 0 } {
+    return
+    }
+
+    set mml_HTTP_RESPONSE_RELEASE [clock clicks -milliseconds]
+
+    # catch if important stats are missing and exit gracefully
+    if { ($mml_HTTP_REQUEST_RELEASE equals 0) || ($mml_ASM_REQUEST_DONE equals 0 ) } {
+
+        log local0. "Stats Collection Skipped. Request likely blocked or ASM bypassed,Start_Client_IP=[IP::client_addr],Start_Client_Port=[TCP::client_port]" 
+
+        return 
+    }
+
+    # Collect additional connection level stats if this is the first http request In this TCP session. 
+    if { $mml_http_request_count equals 1 } {
+
+        set mml_a [expr { $mml_CLIENT_ACCEPTED - $mml_FLOW_INIT } ] ; #measure time spent in Client TCP 3WHS. 
+        set mml_b [expr { $mml_CLIENTSSL_HANDSHAKE - $mml_CLIENTSSL_CLIENTHELLO } ] ; #measure time spent in client side ssl handshake.
+        set mml_c [expr { $mml_ASM_REQUEST_DONE - $mml_HTTP_REQUEST } ] ; #measure time spent in F5 HTTP request processing time. 
+        set mml_d [expr { $mml_SERVER_CONNECTED - $mml_LB_SELECTED } ] ; #measure time spent in Server TCP 3WHS.
+        set mml_e [expr { $mml_SERVERSSL_HANDSHAKE - $mml_SERVERSSL_CLIENTHELLO_SEND } ] ; #measure time spent in side ssl handshake.
+        set mml_f [expr { $mml_HTTP_RESPONSE - $mml_HTTP_REQUEST_RELEASE } ] ; #measure time spent in pool HTTP response latency.
+        set mml_g [expr { $mml_HTTP_RESPONSE_RELEASE - $mml_HTTP_RESPONSE } ] ; #measure time spent in F5 HTTP response processing time. 
+        set mml_overhead [expr { $mml_c + $mml_g } ] ; #Combine HTTP Request and Response F5 processing time 
+
+        if { $mml_enableInsertResponseHeader equals 1 } { 
+            #Insert Server-Timing HTTP header into the HTTP response. Formatting per https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing. These labels can be adjusted as needed.   
+            HTTP::header insert $mml_serverTimingHeaderName "$mml_serverTimingLabel, overhead;dur=$mml_overhead, origin;dur=$mml_f, client-ssl;dur=$mml_b, server-ssl;dur=$mml_e, client-tcp;dur=$mml_a, server-tcp;dur=$mml_d" 
+        }  
+} else {
+        #Only Collect request Level stats when this is not the first http Request in the TCP session.
+        set mml_c [expr { $mml_ASM_REQUEST_DONE - $mml_HTTP_REQUEST } ] ; #measure time spent in F5 HTTP request processing time.
+        set mml_f [expr { $mml_HTTP_RESPONSE - $mml_HTTP_REQUEST_RELEASE } ] ; #measure time spent in pool HTTP response latency.
+        set mml_g [expr { $mml_HTTP_RESPONSE_RELEASE - $mml_HTTP_RESPONSE } ] ; #measure time spent in F5 HTTP response processing time.
+        set mml_overhead [expr { $mml_c + $mml_g } ] ; #Combine HTTP Request and Response F5 processing time 
+
+        if { $mml_enableInsertResponseHeader equals 1 } { 
+            #Insert Server-Timing HTTP header into the HTTP response. Formatting per https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing. These labels can be adjusted as needed.   
+            HTTP::header insert $mml_serverTimingHeaderName "$mml_serverTimingLabel, overhead;dur=$mml_overhead, origin;dur=$mml_f" 
+        } 
+    #Clean up variables for next HTTP request incase there is TCP reuse. 
+    #Dont edit these system Variables 
+    set mml_FLOW_INIT [clock clicks -milliseconds]
+    set mml_CLIENT_ACCEPTED 0
+    set mml_CLIENTSSL_CLIENTHELLO 0
+    set mml_CLIENTSSL_HANDSHAKE 0 
+    set mml_HTTP_REQUEST 0 
+    set mml_ASM_REQUEST_DONE 0 
+    set mml_HTTP_REQUEST_RELEASE 0 
+    set mml_SERVER_CONNECTED 0 
+    set mml_SERVERSSL_CLIENTHELLO_SEND 0 
+    set mml_SERVERSSL_HANDSHAKE 0  
+    set mml_HTTP_RESPONSE 0 
+    set mml_HTTP_RESPONSE_RELEASE 0
+    set mml_debugTiming 0
+}
+}
---
+++ Modular_Functions/modular_traceparent.tcl
@@ -0,0 +1,131 @@
+# Made with ❤ by Matt Stovall 2/2024. Traceparent code borrowed from Granville Schmidt. 
+# This iRule generates a unique traceparent ID and inserts that value as a HTTP request header. 
+#All code is wrapped in catch statements so that any failure will be non-blocking. If making changes to the code, please ensure its still covered by the catch statements. 
+#See https://github.com/megamattzilla/iRules/blob/master/Modular_Functions/README.md for more details
+#Version 1.00
+#Modular iRule dependency: none
+
+# W3C Trace Context specification:
+#   Ref: https://www.w3.org/TR/trace-context-1/
+#   Level: 1
+#   W3C Recommendation 06 February 2020
+#
+# Author: 
+#
+# Automated test harnes: https://github.com/w3c/trace-context/tree/master/test
+#
+# Headers:
+#   traceparent ( https://www.w3.org/TR/trace-context-1/#traceparent-header ):
+#     Fields:
+#       version-format   = trace-id "-" parent-id "-" trace-flags
+#       trace-id         = 32HEXDIGLC  ; 16 bytes array identifier. All zeroes forbidden
+#       parent-id        = 16HEXDIGLC  ; 8 bytes array identifier. All zeroes forbidden
+#       trace-flags       = 2HEXDIGLC   ; 8 bit flags. Currently, only one bit is used. See below for details
+#
+#     EXAMPLE> traceparent: 00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-00
+#
+# Generating a W3C Trace Context 'trace-id'
+#   Ref: https://www.w3.org/TR/trace-context-1/#considerations-for-trace-id-field-generation
+proc generate_trace_id {} {
+  # Trace IDs are 16 random bytes
+  return [call gen_rand_hex_str 16]
+}
+# Generating a W3C Trace Context 'parent-id'
+#   Refs:
+#     - https://www.w3.org/TR/trace-context-1/#parent-id
+#     - https://www.w3.org/TR/trace-context-1/#privacy-of-traceparent-field
+proc generate_parent_id {} {
+  # Parent IDs are 8 random bytes
+  return [call gen_rand_hex_str 8]
+}
+# Generates a random N-byte string
+proc gen_rand_hex_str {num_bytes} {
+  # Seeded via srand() in RULE_INIT
+  for { set i 0 } {$i < $num_bytes * 2} { incr i } {
+    append rand_hex [format {%x} [expr {int(rand()*16)}]]
+  }
+  return $rand_hex
+}
+# Protecting against Denial-of-Service attacks
+#   Ref: https://www.w3.org/TR/trace-context-1/#denial-of-service
+proc is_safe_to_propagate_trace_context {} {
+  return true
+}
+# Generates a new 'traceparent' header
+# Ref: https://www.w3.org/TR/trace-context-1/#traceparent-header
+proc generate_new_traceparent {} {
+  set version [format {%02x} 0x00]
+  set trace_id [call generate_trace_id]
+  set parent_id [call generate_parent_id]
+  set trace_flags [format {%02x} 0x01]
+  return [call format_traceparent $version $trace_id $parent_id $trace_flags]
+}
+proc format_traceparent {version trace_id parent_id trace_flags} {
+  return [format "%s-%s-%s-%s" $version $trace_id $parent_id $trace_flags]
+}
+proc try_propagate_traceparent_header {traceparent_header} {
+  #############################################################################
+  # Ref: https://www.w3.org/TR/trace-context-1/#a-traceparent-is-received     #
+  #############################################################################
+  if {[regexp -- {^\s*([0-9a-f]{2})-\s*([a-f0-9]{16,32})\s*-([a-f0-9]{16})-([a-f0-9]{2})(?:-*)\s*?$} $traceparent_header a b c d e]} then {
+    set version $b
+    set trace_id $c
+    set parent_id $d
+    set trace_flags $e
+    # Version 0xff is invalid.
+    # Refs:
+    #   - https://www.w3.org/TR/trace-context-1/#versioning-of-traceparent
+    #   - https://www.w3.org/TR/trace-context/#a-traceparent-is-received
+    if {$b == "ff"} {
+      return ""
+    }
+    # If the existing "trace-id" uses a shorter identifier, left pad the original identifier with zeroes,
+    # so that the resulting identifier is a compliant 16-byte identifier.
+    #
+    # Ref: https://www.w3.org/TR/trace-context-1/#interoperating-with-existing-systems-which-use-shorter-identifiers
+    set trace_id [format "%0*s" 32 $trace_id]
+    # All bytes as zero (00000000000000000000000000000000) are considered an invalid value. If the trace-id value is
+    # invalid (for example if it contains non-allowed characters or all zeros), vendors MUST ignore the traceparent.
+    if {$trace_id eq "00000000000000000000000000000000"} {
+      return ""
+    }
+    # All bytes as zero (0000000000000000) is considered an invalid value. Vendors MUST ignore the traceparent when the
+    # parent-id is invalid (for example, if it contains non-lowercase hex characters).
+    if {$parent_id eq "0000000000000000"} {
+      return ""
+    }
+    # Update the 'parent-id' with a new identifier for this operation.
+    # Ref: https://www.w3.org/TR/trace-context-1/#a-traceparent-is-received (7.1)
+    set parent_id [call generate_parent_id]
+    return [call format_traceparent $version $trace_id $parent_id $trace_flags]
+  }
+  return ""
+}
+
+
+when HTTP_REQUEST priority 510 {
+catch {
+  if {[HTTP::header count traceparent] == 1 && [call is_safe_to_propagate_trace_context]} then {
+    set traceparent [call try_propagate_traceparent_header [HTTP::header "traceparent"]]
+    if {$traceparent eq ""} {
+      set traceparent [call generate_new_traceparent]
+    }
+    #       Until validated and parsed, pass the header through.
+    if {[HTTP::header exists tracestate] && [HTTP::header tracestate] eq ""} {
+      HTTP::header remove tracestate
+    } else {
+      HTTP::header replace tracestate [string trim [HTTP::header tracestate]]
+    }
+  } else {
+    #############################################################################
+    # Ref: https://www.w3.org/TR/trace-context-1/#no-traceparent-received       #
+    #############################################################################
+    HTTP::header remove tracestate
+    set traceparent [call generate_new_traceparent]
+  }
+  # Append W3C Trace Context Headers
+  # Ref: https://www.w3.org/TR/trace-context-1/#other-risks
+  HTTP::header remove traceparent
+  HTTP::header insert traceparent $traceparent
+}
+}
