1aaf117f7eae0ef1f8394100c088ea01bab472dc
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2024-02-14 15:30:25 -0600

    adding more logic checks

--- SSLO_Sideband/intercept_sideband.tcl
+++ SSLO_Sideband/intercept_sideband.tcl
@@ -7,95 +7,137 @@
 
 #All code is wrapped in catch statements so that any failure will be non-blocking. If making changes to the code, please ensure its still covered by the catch statements. A prefix of z1_ has been added to each variable to make them globally unique. 
 #See https://github.com/megamattzilla/iRules/blob/master/SSLO_Sideband/README.md for more details
-#Version 1.0
+#Version 1.1
 #Requirements: TBD
 
-when HTTP_REQUEST { 
-## Set request URL 
-sharedvar THIS_HOST 
-set THIS_HOST [HTTP::uri] 
-## parse incoming request URL 
-       if { ${THIS_HOST} starts_with "http://" } {  
-           set this_url [string trimright [string trimleft ${THIS_HOST} "http://"] "/"]  
+when HTTP_REQUEST priority 200 { 
+
+    #TODO: add variables including custom string in message.
+    ###User-Edit Variables start###
+    set is_sidebandPool "mcafee-api" ; #LTM pool containing nodes to send the sideband HTTP request
+    ###User-Edit Variables end###
+
+## Set variable containing explicit proxy request URI (this contains full http://fqdn/uri per the RFC) 
+set is_httpURI [HTTP::uri] 
+
+## Set variable containing just FQDN without port or scheme
+       if { ${is_httpURI} starts_with "http://" } {  
+           set is_httpHost [string trimright [string trimleft ${is_httpURI} "http://"] "/"]  
        } else { 
-           set this_url [findstr ${THIS_HOST} "" 0 ":"]     
+           set is_httpHost [findstr ${is_httpURI} "" 0 ":"]     
        } 
 
-#Check if purge cache header exists. If true, delete the cache entry. Note: this HTTP header would be on the explicit proxy request (CONNECT) which is uncommon.   
+## Check if purge cache header exists. If true, delete the cache entry. Note: this HTTP header would be on the explicit proxy request (CONNECT) which is uncommon.   
 if { [HTTP::header exists "X-SSLO-URL-PURGE"] } {
-    log local0. "PURGE cache $this_url" 
-    table delete $this_url 
+    log local0. "PURGE cache $is_httpHost" 
+    table delete $is_httpHost 
 }
 
-#Check if this is a CONNECT request to see if its necessary to lookup URL category for SSL bypass decisions. 
-if { [HTTP::method] equals "CONNECT" } { 
+## Check if this is a CONNECT request to see if its necessary to lookup URL category for SSL bypass decisions. Exit gracefully if its not a CONNECT request.
+if { !([HTTP::method] equals "CONNECT") } {
+    return 
+} 
 
-    #Perform iRule cache lookup for $this_urll
-    set cachelookup [table lookup $this_url]
+## Perform iRule cache lookup for $is_httpHost
+    set is_cacheLookup [table lookup $is_httpHost]
 
-    #Check if iRule table has entry for $this_url.  Expecting data to contain "Bypass" or "Intercept".  If found, set X_CATEGORY and X_DECRYPT_DECISION using table data. Skip calling sideband.  
-    if { $cachelookup contains "Bypass"  or $cachelookup contains "Intercept" } {
-        log local0. "HIT cache $this_url = $cachelookup"  
-        #Set decrypt decision based on cached iRule table data 
-        set X_DECRYPT_DECISION [getfield $cachelookup "|" 1]
-        set X_CATEGORY [getfield $cachelookup "|" 2]
+## Check if  cache (iRule table) has entry for $is_httpHost.  Expecting data to contain "Bypass" or "Intercept".  If found, set X_CATEGORY and X_DECRYPT_DECISION using table data. Then skip calling the sideband.  
+if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" } {
+    ## Cache HIT
+    log local0. "HIT cache $is_httpHost = $is_cacheLookup"  
+    #Set decrypt decision based on cached iRule table data 
+    set X_DECRYPT_DECISION [getfield $is_cacheLookup "|" 1]
+    set X_CATEGORY [getfield $is_cacheLookup "|" 2]
 
-    } else {
-        log local0. "MISS cache $this_url = $cachelookup"  
-        ## issue MWG API sideband call 
+} else {
+
+    ## Cache MISS
+    log local0. "MISS cache $is_httpHost = $is_cacheLookup"  
+
+    ## Get the list of active members in the sideband pool  
+    set is_members [active_members -list $is_sidebandPool] 
+
+    ## Count the number of members  
+    set is_count [llength $is_members]
+    
+    ## Check if there are any healthy nodes. If there is no healthy nodes, log and exit gracefully. 
+    if { $is_count equals 0 } { 
+        log local0. "CRITICAL no healthy nodes in sideband pool!" 
+        return
+        }
+
+    ## Select a random member index from the pool 
+    set is_randomMemberIndex [expr {int(rand()*$is_count)}]  
 
 
-      # Get the list of active members in the pool  
-      set members [active_members -list mcafee-api]  
-      #Count the number of members  
-      set count [llength $members]
-      if { $count equals 0 } { log local0. "CRITICAL no healthy nodes in sideband pool!" }
-      # Select a random member  
-      set random_member_index [expr {int(rand()*$count)}]  
-      # Get the member  
-      set member_to_use "[getfield [lindex $members $random_member_index] " " 1]:2082" 
-      log local0. "$member_to_use"
+    ## Format IP:port for the member that is selected.    
+    set member_to_use "[string map {" " ":"}[lindex $is_members $is_randomMemberIndex]]"
 
+    
+    ## Open the TCP connection to the sideband pool member
+    set conn_id [connect -timeout 100 -idle 30 -status conn_status $member_to_use] 
 
+    ## Check if TCP connection was successful. If not, try again with a different pool member.  
+    if { !($conn_id contains "connect") } { 
+        log local0. "CRITICAL cannot connect sideband to $member_to_use. Retrying."
+        
+        ## In a pool with 2 or more active sideband nodes try 10 times to select a different pool member or give up.
+        if { $is_count >= 2 } { 
+            ## Set a loop control variable to 0     
+            set is_loop 0 
+            ## While the loop control variable is less than 10, keep looping. 
+            while { $is_loop < 10 } { 
+                ## Select another random member index from the pool 
+                set is_nextRandomMemberIndex [expr {int(rand()*$is_count)}]
+                ## Check if this new random member is the same as the previous member. 
+                if {($is_nextRandomMemberIndex != $is_randomMemberIndex)} { 
+                    ## A new sideband pool member has been selected. Set variables and Stop looping.
+                    ## Format IP:port for the member that is selected.    
+                    set member_to_use "[string map {" " ":"}[lindex $is_members $is_nextRandomMemberIndex]]"
+                    log local0. "Selected new sideband member $member_to_use on attempt $is_loop"
+                    break
+                ## If this new random member is the same as the previous member, keep looping.     
+                } else { incr is_loop }
+            }
+        }
 
-        set conn_id [connect -timeout 100 -idle 30 -status conn_status $member_to_use] ; ## Use MWG IP and appropriate port
-        log local0. "conn_id= $conn_id"
+    
+        ## Open the RETRY TCP connection to the sideband pool member
+        set conn_id [connect -timeout 100 -idle 30 -status conn_status $member_to_use] 
+
+        ## Check if the RETRY TCP connection was successful. If not, log a message and exit gracefully.   
         if { !($conn_id contains "connect") } { 
-            log local0. "CRITICAL cannot connect sideband to $member_to_use"
-            virtual sslo_noAuth.app/sslo_noAuth-xp-4
-            return
+            log local0. "CRITICAL cannot connect sideband to $member_to_use after RETRY."
+            return 
+        }  
+    }
+     
+    set data "HEAD /?url=${is_httpHost} HTTP/1.1\r\nHost: 10.5.20.245\r\n\r\n" 
+    set send_bytes [send -timeout 500 -status send_status $conn_id $data]
+    log local0. "send_bytes= $send_bytes"
+    #TODO: caculate send bytes to compare. 
+    if { !($send_bytes >= "2") } { 
+        log local0. "CRITICAL unable to send sideband call to $member_to_use"
+        return
+    }
+
+    set recv_data [recv -peek -timeout 500 -status recv_status $conn_id]
+    log local0. "recv_data= $recv_data"
+    #TODO: add log for empty response vs invalid. and log invalid response 
+    if { !($recv_data contains "Bypass") and !($recv_data contains "Intercept") } { 
+        log local0. "CRITICAL invalid sideband response from $member_to_use"
+        return
         }
 
-        set data "HEAD /?url=${this_url} HTTP/1.1\r\nHost: 10.5.20.245\r\n\r\n" 
-        set send_bytes [send -timeout 500 -status send_status $conn_id $data]
-        log local0. "send_bytes= $send_bytes"
-        if { !($send_bytes >= "2") } { 
-            log local0. "CRITICAL unable to send sideband call to $member_to_use"
-            virtual sslo_noAuth.app/sslo_noAuth-xp-4
-            return
-        }
-        
-        set recv_data [recv -peek -timeout 500 -status recv_status $conn_id]
-        log local0. "recv_data= $recv_data"
-        if { !($recv_data contains "Bypass") and !($recv_data contains "Intercept") } { 
-            log local0. "CRITICAL invalid sideband response from $member_to_use"
-            virtual sslo_noAuth.app/sslo_noAuth-xp-4
-            return
-            }
-        
-        ## parse sideband results 
-        set X_CATEGORY [findstr [lsearch -inline [split $recv_data "\r\n"] X-Categories*] ": " 2] 
-        set X_DECRYPT_DECISION [findstr [lsearch -inline [split $recv_data "\r\n"] X-Decrypt-Decision*] ": " 2] 
-        table set $this_url "$X_DECRYPT_DECISION|$X_CATEGORY" 3600 3600 
-        log local0. "$THIS_HOST X_DECRYPT_DECISION: $X_DECRYPT_DECISION  X_CATEGORY: $X_CATEGORY"
-        if { ${X_DECRYPT_DECISION} eq "Intercept" } { 
-            virtual sslo_noAuth.app/sslo_noAuth-xp-4 
-            }
-        if { ${X_DECRYPT_DECISION} eq "Bypass" } { 
-                virtual sslo_noAuth.app/sslo_noAuth-xp-4
-                }           
+    ## parse sideband results 
+    set X_CATEGORY [findstr [lsearch -inline [split $recv_data "\r\n"] X-Categories*] ": " 2] 
+    set X_DECRYPT_DECISION [findstr [lsearch -inline [split $recv_data "\r\n"] X-Decrypt-Decision*] ": " 2] 
+    table set $is_httpHost "$X_DECRYPT_DECISION|$X_CATEGORY" 3600 3600 
+    log local0. "$is_httpURI X_DECRYPT_DECISION: $X_DECRYPT_DECISION  X_CATEGORY: $X_CATEGORY"    
+    
 
 }
-}
-virtual sslo_noAuth.app/sslo_noAuth-xp-4
+    if { ${X_DECRYPT_DECISION} eq "Bypass" } { 
+            virtual sslo_noAuth.app/sslo_noAuth-xp-4
+            }       
 }
