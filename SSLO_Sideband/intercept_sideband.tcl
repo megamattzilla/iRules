73d5a97f549327bd061eab190b01974a58d6d843
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2024-02-16 15:17:28 -0600

    V1.3

--- SSLO_Sideband/intercept_sideband.tcl
+++ SSLO_Sideband/intercept_sideband.tcl
@@ -1,22 +1,33 @@
-# Made with care by Matt Stovall 2/2024.
-#This iRule: 
-#   1.   collects HTTP information (HTTP host FQDN) from an explicit proxy HTTP request
-#   2.   makes a sideband HTTP call to a HTTP proxy with this information
-#   3.   inspects HTTP response from HTTP proxy for HTTP headers indicating the explicit proxy request should be SSL intercepted
-#   4.   based on that response, send the explicit proxy HTTP request to the appropriate virtual server that either intercepts or bypasses SSL decryption
+## Made with care by Matt Stovall 2/2024.
+## Version 1.3
+## This iRule: 
+##  1.   collects HTTP information (HTTP host FQDN) from an explicit proxy HTTP request
+##  2.   makes a sideband HTTP call to a HTTP proxy with this FQDN information in the URI as a query string. (/?url=${is_httpHost})  
+##  3.   inspects HTTP response from HTTP proxy for HTTP headers indicating the explicit proxy request should be SSL intercepted
+##  4.   based on that response, send the explicit proxy HTTP request to the appropriate virtual server that either intercepts or bypasses SSL decryption
 
-#All code is wrapped in catch statements so that any failure will be non-blocking. If making changes to the code, please ensure its still covered by the catch statements. A prefix of z1_ has been added to each variable to make them globally unique. 
-#See https://github.com/megamattzilla/iRules/blob/master/SSLO_Sideband/README.md for more details
-#Version 1.2
-#Requirements: TBD
+## All code is wrapped in catch statements so that any failure will be non-blocking. If making changes to the code, please ensure its still covered by the catch statements. A prefix of is_ has been added to each variable to make them globally unique. 
+## See https://github.com/megamattzilla/iRules/blob/master/SSLO_Sideband/README.md for more details
+
+## Requirements: 
+##  1. To be applied to a vip-targeting-vip which points to an explicit proxy virtual server.
+##  2. Configure a LTM pool with your sideband pool members.
+##  3. Configure those sideband pool members to reply with the Bypass/Intercept strings this iRule is looking for.   
 
 when HTTP_REQUEST priority 200 { 
 
-    #TODO: add variables including custom string in message.
     ###User-Edit Variables start###
     set is_sidebandPool "mcafee-api" ; #LTM pool containing nodes to send the sideband HTTP request
     set is_debugLogging 1 ; #0 = Disabled, 1 = Enabled
-
+    set is_errorLoggingString "CRIT:" ; #Keyword string to be included in error logs. 
+    set is_cacheTimeout 3600 ; #Number of seconds to cache a bypass/intercept decision for a given FQDN. 
+    set is_cachePurgeHeader "X-SSLO-URL-PURGE" ; #HTTP request header name that will purge the cache for a given FQDN
+    set is_sslBypassVirtualServer "sslo_noAuth.app/sslo_noAuth-xp-4" ; #Virtual server path/name to send explicit proxy requests that should NOT be SSL intercepted. 
+    set is_sidebandHostHeader "sideband.example.com" ; #HTTP host header value to use in the sideband call.
+    set is_sidebandConnectTimeout 50 ; #the time in milliseconds to wait to establish the connection to the sideband pool member.    
+    set is_sidebandIdleTimeout 30 ; #the time in seconds to leave the connection open if it is unused.
+    set is_sidebandSendTimeout 50 ; #the time in milliseconds to transmit the HTTP request to the sideband pool member.  
+    set is_sidebandReceiveTimeout 30 ; #the time in milliseconds to wait for the HTTP response to be received from the sideband pool member. 
     ###User-Edit Variables end###
 
 ## Set variable containing explicit proxy request URI (this contains full http://fqdn/uri per the RFC) 
@@ -30,7 +41,7 @@ set is_httpURI [HTTP::uri]
        } 
 
 ## Check if purge cache header exists. If true, delete the cache entry. Note: this HTTP header would be on the explicit proxy request (CONNECT) which is uncommon.   
-if { [HTTP::header exists "X-SSLO-URL-PURGE"] } {
+if { [HTTP::header exists $is_cachePurgeHeader] } {
     if { $is_debugLogging == 1 } { log local0. "DEBUG: PURGE cache $is_httpHost" } 
     table delete $is_httpHost 
 }
@@ -43,13 +54,13 @@ if { !([HTTP::method] equals "CONNECT") } {
 ## Perform iRule cache lookup for $is_httpHost
     set is_cacheLookup [table lookup $is_httpHost]
 
-## Check if  cache (iRule table) has entry for $is_httpHost.  Expecting data to contain "Bypass" or "Intercept".  If found, set X_CATEGORY and X_DECRYPT_DECISION using table data. Then skip calling the sideband.  
+## Check if  cache (iRule table) has entry for $is_httpHost.  Expecting is_data to contain "Bypass" or "Intercept".  If found, set is_urlCategory and is_decryptDecision using table is_data. Then skip calling the sideband.  
 if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" } {
     ## Cache HIT
        if { $is_debugLogging == 1 } { log local0. "DEBUG: HIT cache $is_httpHost = $is_cacheLookup" }
-    #Set decrypt decision based on cached iRule table data 
-    set X_DECRYPT_DECISION [getfield $is_cacheLookup "|" 1]
-    set X_CATEGORY [getfield $is_cacheLookup "|" 2]
+    #Set decrypt decision based on cached iRule table is_data 
+    set is_decryptDecision [getfield $is_cacheLookup "|" 1]
+    set is_urlCategory [getfield $is_cacheLookup "|" 2]
 
 } else {
 
@@ -64,7 +75,7 @@ if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" 
     
     ## Check if there are any healthy nodes. If there is no healthy nodes, log and exit gracefully. 
     if { $is_count equals 0 } { 
-        log local0. "CRIT: no healthy nodes in sideband pool!" 
+        log local0. "$is_errorLoggingString no healthy nodes in sideband pool!" 
         return
         }
 
@@ -73,15 +84,15 @@ if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" 
 
 
     ## Format IP:port for the member that is selected.    
-    set member_to_use "[string map {" " ":"}[lindex $is_members $is_randomMemberIndex]]"
+    set is_memberToUse "[string map {" " ":"}[lindex $is_members $is_randomMemberIndex]]"
 
     
     ## Open the TCP connection to the sideband pool member
-    set conn_id [connect -timeout 100 -idle 30 -status conn_status $member_to_use] 
+    set is_connID [connect -timeout $is_sidebandConnectTimeout -idle $is_sidebandIdleTimeout -status conn_status $is_memberToUse] 
 
     ## Check if TCP connection was successful. If not, try again with a different pool member.  
-    if { !($conn_id contains "connect") } { 
-        log local0. "CRIT: cannot connect sideband to $member_to_use. Retrying."
+    if { !($is_connID contains "connect") } { 
+        log local0. "$is_errorLoggingString cannot connect sideband to $is_memberToUse. Retrying."
         
         ## In a pool with 2 or more active sideband nodes try 10 times to select a different pool member or give up and try the same pool member.
         if { $is_count >= 2 } { 
@@ -95,8 +106,8 @@ if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" 
                 if {($is_nextRandomMemberIndex != $is_randomMemberIndex)} { 
                     ## A new sideband pool member has been selected. Set variables and Stop looping.
                     ## Format IP:port for the member that is selected.    
-                    set member_to_use "[string map {" " ":"}[lindex $is_members $is_nextRandomMemberIndex]]"
-                    if { $is_debugLogging == 1 } { log local0. "DEBUG: Selected new sideband member $member_to_use on attempt $is_loop" }
+                    set is_memberToUse "[string map {" " ":"}[lindex $is_members $is_nextRandomMemberIndex]]"
+                    if { $is_debugLogging == 1 } { log local0. "DEBUG: Selected new sideband member $is_memberToUse on attempt $is_loop" }
                     break
                 ## If this new random member is the same as the previous member, keep looping.     
                 } else { incr is_loop }
@@ -105,55 +116,55 @@ if { $is_cacheLookup contains "Bypass"  or $is_cacheLookup contains "Intercept" 
 
     
         ## Open the RETRY TCP connection to the sideband pool member
-        set conn_id [connect -timeout 100 -idle 30 -status conn_status $member_to_use] 
+        set is_connID [connect -timeout $is_sidebandConnectTimeout -idle $is_sidebandIdleTimeout -status conn_status $is_memberToUse] 
 
         ## Check if the RETRY TCP connection was successful. If not, log a message and exit gracefully.   
-        if { !($conn_id contains "connect") } { 
-            log local0. "CRIT: cannot connect sideband to $member_to_use after RETRY."
+        if { !($is_connID contains "connect") } { 
+            log local0. "$is_errorLoggingString cannot connect sideband to $is_memberToUse after RETRY."
             return 
         }  
     }
 
     ## Format HTTP Request to sideband with Host FQDN from the explicit proxy request  
-    set data "HEAD /?url=${is_httpHost} HTTP/1.1\r\nHost: 10.5.20.245\r\n\r\n" 
+    set is_data "HEAD /?url=${is_httpHost} HTTP/1.1\r\nHost: $is_sidebandHostHeader\r\n\r\n" 
 
     ## Send HTTP Request to sideband pool member 
-    set send_bytes [send -timeout 500 -status send_status $conn_id $data]
+    set is_sendBytes [send -timeout $is_sidebandSendTimeout -status send_status $is_connID $is_data]
     
     ## Check HTTP Request was sent successfully 
-    if { $is_debugLogging == 1 } {  log local0. "DEBUG: Sent $send_bytes bytes out of [string length $data] bytes" }
-    if { !($send_bytes == [string length $data]) } { 
-        log local0. "CRIT: unable to send sideband call to $member_to_use. Sent $send_bytes bytes out of [string length $data] bytes"
+    if { $is_debugLogging == 1 } {  log local0. "DEBUG: Sent $is_sendBytes bytes out of [string length $is_data] bytes" }
+    if { !($is_sendBytes == [string length $is_data]) } { 
+        log local0. "$is_errorLoggingString unable to send sideband call to $is_memberToUse. Sent $is_sendBytes bytes out of [string length $is_data] bytes"
         return
     }
 
     ## Check HTTP Response was received from sideband pool member
-    set recv_data [recv -peek -timeout 500 -status recv_status $conn_id]
-    if { $is_debugLogging == 1 } {  log local0. "DEBUG: Received Response: $recv_data" }
+    set is_recv_data [recv -peek -timeout $is_sidebandReceiveTimeout -status recv_status $is_connID]
+    if { $is_debugLogging == 1 } {  log local0. "DEBUG: Received Response: $is_recv_data" }
     
     ## Check if response is empty
-    if { [string length $recv_data] <= 1 } { 
-        log local0. "CRIT: empty response from $member_to_use"
+    if { [string length $is_recv_data] <= 1 } { 
+        log local0. "$is_errorLoggingString empty response from $is_memberToUse"
         return
         }
     
     ## Check if response does not contain expected strings     
-    if { !($recv_data contains "Bypass") and !($recv_data contains "Intercept") } { 
-        log local0. "CRIT: invalid sideband response from $member_to_use"
+    if { !($is_recv_data contains "Bypass") and !($is_recv_data contains "Intercept") } { 
+        log local0. "$is_errorLoggingString invalid sideband response from $is_memberToUse"
         return
         }
 
     ## Add sideband results to variables 
-    set X_CATEGORY [findstr [lsearch -inline [split $recv_data "\r\n"] X-Categories*] ": " 2] 
-    set X_DECRYPT_DECISION [findstr [lsearch -inline [split $recv_data "\r\n"] X-Decrypt-Decision*] ": " 2] 
+    set is_urlCategory [findstr [lsearch -inline [split $is_recv_data "\r\n"] X-Categories*] ": " 2] 
+    set is_decryptDecision [findstr [lsearch -inline [split $is_recv_data "\r\n"] X-Decrypt-Decision*] ": " 2] 
     
     ## Set cache entry for this HTTP FQDN
-    table set $is_httpHost "$X_DECRYPT_DECISION|$X_CATEGORY" 3600 3600 
-    if { $is_debugLogging == 1 } { log local0. "DEBUG: Added Cache entry $is_httpURI X_DECRYPT_DECISION: $X_DECRYPT_DECISION  X_CATEGORY: $X_CATEGORY" }
+    table set $is_httpHost "$is_decryptDecision|$is_urlCategory" $is_cacheTimeout $is_cacheTimeout
+    if { $is_debugLogging == 1 } { log local0. "DEBUG: Added Cache entry $is_httpURI is_decryptDecision: $is_decryptDecision  is_urlCategory: $is_urlCategory" }
 
 }
     ## If HTTP FQDN should be bypassed, send the explicit proxy HTTP request to a different virtual server. 
-    if { ${X_DECRYPT_DECISION} eq "Bypass" } { 
-        virtual sslo_noAuth.app/sslo_noAuth-xp-4
+    if { ${is_decryptDecision} eq "Bypass" } { 
+        virtual $is_sslBypassVirtualServer
     }       
 }
