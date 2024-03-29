fef429bc4fc21edf1e9519db2519cc63e01c6ce2
megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2024-03-29 10:54:23 -0500

    0.6

--- SSLO_Sideband_SCP/apm.tcl
+++ SSLO_Sideband_SCP/apm.tcl
@@ -10,24 +10,59 @@
 ##  1. TBD 
 
 when HTTP_REQUEST priority 400 {
-#log local0. "plz work "
+log local0. "plz work "
 #set insert_uuid_header 0 
 ECA::disable
+set user_key "unknown:unknown" 
 set user_key "[HTTP::header value X-Authenticated-User]:[HTTP::header value X-Client-IP]"
 
-set user_uuid [ ACCESS::user getsid $user_key ]
+set test [ ACCESS::user getsid $user_key ]
 
-if {  [string length $user_uuid ] >= 10 } { 
-log local0. "Found existing session for $user_key = $user_uuid. Skipping APM."
-sharedvar APMSID
-set APMSID $user_uuid 
-ACCESS::disable
+if {  [string length $test ] >= 10 } { 
+    log local0. "Found existing APM session for $user_key = $test. Skipping APM."
+    sharedvar APMSID
+    set APMSID $test
+    ## Increment bytes in
+    set bytes [ACCESS::session data get -sid $APMSID "session.stats.bytes.in"]
+    set newbytes [expr { [string length [HTTP::request]] + $bytes }]
+    log local0. "IN: Old bytes $bytes new bytes $newbytes"
+    ACCESS::session data set -sid $APMSID "session.stats.bytes.in" $newbytes 
+    ACCESS::disable
+
+} else { 
+    ## Check if session is in progress
+    if { [table lookup $user_key] == 1 } {
+        log local0. "Session in-progress for $user_key. Adding this request to the que. "
+        ## Check for session to finish in a loop 
+        ## Set a loop control variable to 0     
+        set loop 0
+        while { $loop < 100 } { 
+            incr loop
+            ## Sleep 500 miliseconds
+            after 500
+            ## Check if APM session is finished now
+            set test [ ACCESS::user getsid $user_key ]
+            if {  [string length $test ] >= 10 } { 
+                log local0. "Found existing APM session for $user_key = $test. Flushing request from que."
+                sharedvar APMSID
+                set APMSID $test 
+                ACCESS::disable
+                break
+            } else { continue } 
+        }
+    } else {
+        ## New APM session 
+        log local0. "Starting new session for $user_key"
+        table set $user_key 1 
+    }
 }
 }
 
 
 
 when ACCESS_SESSION_STARTED {
+log local0. "sleeping"
+after 5000
 log local0. "Starting session for $user_key"
 ACCESS::session data set session.user.clientip [HTTP::header value X-Client-IP]
 ACCESS::session data set session.custom.scp.groups [HTTP::header value X-Authenticated-UserGroups]
@@ -38,4 +73,24 @@ ACCESS::session data set session.login.last.originalIP [IP::client_addr]
 ACCESS::session data set session.user.uuid $user_key
 ACCESS::session data set session.ntlm.last.result 1
 #ACCESS::session data set session.user.ip_session apm.session.ip.192.168.254.100/Common/scp
+}
+
+when ACCESS_POLICY_COMPLETED { 
+table delete $user_key 
+}
+
+when HTTP_RESPONSE { 
+log local0. "plz work "
+
+
+
+if {  [info exists APMSID] } { 
+    log local0. "Found existing APM session for $user_key = $APMSID"
+    ## Increment bytes in
+    set rbytes [ACCESS::session data get -sid $APMSID "session.stats.bytes.out"]
+    set rnewbytes [expr { [string length [HTTP::response]] + $rbytes }]
+    log local0. "OUT: Old bytes $rbytes new bytes $rnewbytes"
+    ACCESS::session data set -sid $APMSID "session.stats.bytes.out" $rnewbytes 
+
+}
 }
